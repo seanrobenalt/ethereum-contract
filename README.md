2086c91e2731780cd9a09bdaef5ffd35ba74b9aa
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-01-22 10:52:58 -0500

    coin and helloworld

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ download go-ethereum with homebrew
 run `geth account new` to create an account on local node
 
 `geth` will then connect you to the network
+
+# tools
+
+[solidity docs](https://solidity.readthedocs.io/en/develop/introduction-to-smart-contracts.html#simple-smart-contract)
+
+[remix ide](https://github.com/ethereum/remix)
+
+[go-ethereum](https://github.com/ethereum/go-ethereum)
+
+[mist](https://github.com/ethereum/mist/releases)
---
+++ coin.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.16;
+
+// Track transactions with something like this
+Coin.Sent().watch({}, '', function(error, result) {
+    if (!error) {
+        console.log("Coin transfer: " + result.args.amount +
+            " coins were sent from " + result.args.from +
+            " to " + result.args.to + ".");
+        console.log("Balances now:\n" +
+            "Sender: " + Coin.balances.call(result.args.from) +
+            "Receiver: " + Coin.balances.call(result.args.to));
+    }
+})
+
+// create a coin
+contract Coin {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    function Coin() public {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Sent(msg.sender, receiver, amount);
+    }
+}
