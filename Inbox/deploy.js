a5e1371a67097841e7f500fe7f2bb43b911bc119
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-01-30 22:28:50 -0500

    finish deploy

--- Inbox/deploy.js
+++ Inbox/deploy.js
@@ -7,3 +7,14 @@ const provider = new HDWalletProvider(
   'https://rinkeby.infura.io/qwbfIOWRoRNZORuEcnrS'
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['whatup fool'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
