558f2b8d4e007abab3b8d666ae091a4086adf412
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-02-01 21:48:22 -0500

    require payment

--- Lottery/contracts/Lottery.sol
+++ Lottery/contracts/Lottery.sol
@@ -1,13 +1,15 @@
 pragma solidity ^0.4.17;
 
 contract Lottery {
-    address public manager;
+  address public manager;
 
-    function Lottery() public {
-        manager = msg.sender;
-    }
+  function Lottery() public {
+    manager = msg.sender;
+  }
 
-    function enter() public payable {
-        players.push(msg.sender);
-    }
+  function enter() public payable {
+    require(msg.value > .01 ether);
+
+    players.push(msg.sender);
+  }
 }
