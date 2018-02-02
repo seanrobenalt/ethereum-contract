cc7fe87c1dbb9ce19b641f659a5a53d195aac52a
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-02-01 23:14:41 -0500

    test boilerplate setup for Lottery contract

---
+++ Lottery/deploy.js
@@ -0,0 +1,20 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'bread shrimp devote gorilla catalog sand job silver connect duck horn circle',
+  'https://rinkeby.infura.io/qwbfIOWRoRNZORuEcnrS'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
