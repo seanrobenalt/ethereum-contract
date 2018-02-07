25f7430fe92ef7fb72cfdc41dae76c9c39ce1925
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-02-07 17:41:22 -0500

    first test passing- Lottery contract

--- Lottery/contracts/Lottery.sol
+++ Lottery/contracts/Lottery.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
   address public manager;
+  address[] public players;
 
   function Lottery() public {
     manager = msg.sender;
--- Lottery/test/Lottery.test.js
+++ Lottery/test/Lottery.test.js
@@ -1,12 +1,13 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
-let lottery;
 let accounts;
+let lottery;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
@@ -14,4 +15,11 @@ beforeEach(async () => {
   lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
+    lottery.setProvider(provider);
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
 });
