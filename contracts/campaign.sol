de9cf9cb365cee5d50da344c85b482d7224db338
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-02-11 13:08:44 -0500

    add factory for security and keeping track

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.4.17;
 
+contract CampaignFactory {
+  address[] public deployedCampaigns;
+
+  function createCampaign(uint minimum) public {
+    address newCampaign = new Campaign(minimum, msg.sender);
+    deployedCampaigns.push(newCampaign);
+  }
+
+  function getDeployedCampaigns() public view returns (address[]) {
+    return deployedCampaigns;
+  }
+}
+
 contract Campaign {
   struct Request {
     string description;
@@ -8,21 +21,21 @@ contract Campaign {
     bool complete;
     uint approvalCount;
     mapping(address => bool) approvals;
-    uint public approversCount;
   }
 
   Request[] public requests;
   address public manager;
   uint public minimumContribution;
   mapping(address => bool) public approvers;
+  uint public approversCount;
 
   modifier restricted() {
     require(msg.sender == manager);
     _;
   }
 
-  function Campaign(uint minimum) public {
-    manager = msg.sender;
+  function Campaign(uint minimum, address creator) public {
+    manager = creator;
     minimumContribution = minimum;
   }
 
