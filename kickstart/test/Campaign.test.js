1296eace2558b610daaff888bb9c36143581a95d
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-02-11 18:29:04 -0500

    end to end test

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -77,4 +77,37 @@ describe('Campaigns', () => {
     assert.equal('100', request.value);
     assert.equal(accounts[1], request.recipient);
   });
+
+  it('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    });
+
+    await campaign.methods
+      .createRequest('descriptor here', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+
+    // for viewing balance
+    // console.log(balance);
+    
+    assert(balance > 104);
+  });
 });
