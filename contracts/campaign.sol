31991d84212d130efbef18c519221bc663e586a2
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-02-11 12:24:43 -0500

    approveRequest()

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -42,4 +42,14 @@ contract Campaign {
 
       requests.push(newRequest);
   }
+
+  function approveRequest(uint index) public {
+    Request storage request = requests[index];
+
+    require(approvers[msg.sender]);
+    require(!request.approvals[msg.sender]);
+
+    request.approvals[msg.sender] = true;
+    request.approvalCount++;
+  }
 }
